53cefbb341d744cf4a1c7aff2cf233ffa46356ad
ankit0pokhriyal <62211049+ankit0pokhriyal@users.noreply.github.com>
2024-07-02 07:06:34 +0530

    Add files via upload

---
+++ ArrayTraverse.c
@@ -0,0 +1,21 @@
+// Traversing of an array
+// Traversing of elements of array
+#include<stdio.h>
+int main(){
+    int i, n, arr[20];
+    printf("Enter the No. of elements:");
+    _________________                        // Use scanf  eg. scanf("%d", &n);   
+    ___________________                        // run for loop
+    {
+        _____________________         //  use print command
+        ______________________                //  use adress of array to enter the values eg &arr[i]
+
+    }
+    printf("\n Single ELement arr[3] =" ______________);
+    printf("\n array elements are");
+    ___________________________                // use for loop for entering the element
+    {
+        ____________________________
+    }
+    return 0;
+}
